a3dc08a2bcc8a4b11e18557993ca0dec0b471224
damstaylor <damiensabo@live.fr>
2023-12-12 10:06:08 +0100

    Display first level JSON data in b-table

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
-    <code v-if="json">{{ json }}</code>
+    <b-container>
+      <span v-if="!isLoaded">Loading...</span>
+      <b-table v-else :items="scopesArray" :fields="fields">
+
+      </b-table>
+    </b-container>
   </div>
 </template>
 
@@ -9,17 +14,41 @@ export default {
   name: 'App',
   data() {
     return {
+      isLoaded: false,
       json: null,
+      fields: ['_key', 'claim_type', 'desc_2', 'examples'],
     }
   },
   mounted() {
     this.loadJSON()
+  },
+  computed: {
+    jsonData() {
+      return this.json?.data || null
+    },
+    scopes() {
+      return this.jsonData?.scopes || null
+    },
+    scopesArray() {
+      return this.scopes ? Object.entries(this.scopes).map(item => ({
+          _key: item[0],
+          ...item[1]
+        })
+      ) : []
+    },
+    deprecated() {
+      return this.jsonData?.deprecated || null
+    },
+    markers() {
+      return this.jsonData?.markers || null
+    },
   },
   methods: {
     async loadJSON() {
       try {
         const response = await fetch('https://app.m-itrust.com/v2/public/claims')
         this.json = await response.json()
+        this.isLoaded = true
       } catch (error) {
         console.error('Failed fetching JSON grammar file', error.message)
       }
