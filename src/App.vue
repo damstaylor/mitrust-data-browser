1b28b3e0885f404609283fc5a789b49cf06bb256
damstaylor <damiensabo@live.fr>
2023-12-15 12:55:11 +0100

    Remove console.log

--- src/App.vue
+++ src/App.vue
@@ -53,14 +53,12 @@ export default {
   methods: {
     // { key1: value1, key2: value2, ... } => [ { _key: key1, value1, _showDetails: false }, ... ]
     formatObjectAsArray(obj) {
-      let res = Object.entries(obj).map(item => ({
+      return Object.entries(obj).map(item => ({
             _key: item[0],
             ...item[1],
             _showDetails: false
           })
       )
-      console.log(res)
-      return res
     },
     async loadJSON() {
       try {
